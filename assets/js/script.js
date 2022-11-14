0a12f9aaad0c382ce8065d595ad282cdcb19ad4e
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-14 18:18:04 +0100

    first method working progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,30 @@ var app = new Vue(
 
         data: {
 
+            toDoList: [
+                {
+                    text: `to brush my dragon`,
+                    done: false,
+                },
+                {
+                    text: `to pet my pets`,
+                    done: false,
+                },
+            ],
+
+            doneList: [],
+
         },
 
         methods: {
+
+            markHowDone( index, element ){
+                //boolean Var changes
+                this.toDoList[index].done = true;
+                console.log(`done is`, this.toDoList[index].done);
+                //task goes out from the array
+                
+            }
 
         },
     }
